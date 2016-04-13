d5e01e1d991b3bcd8cf3c754cd9411802baab212
Gabriel Godoy <ggodoyoliveira@gmail.com>
2016-04-12 22:29:00 -0300

    Setup new machine with brews cask

---
+++ setup-machine.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# Brew packages that I use alot.
+#
+brew install wget
+brew install node
+brew install imagemagick
+#
+# Some cask packages that I like.
+#
+brew cask install sublime-text
+brew cask install macvim
+brew cask install iterm2
+brew cask install sketch
+brew cask install dropbox
+brew cask install skitch
+brew cask install firefox
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install livereload
+brew cask install bettertouchtool
+brew cask install atext
+brew cask install alfred
+brew cask install divvy
+brew cask install caffeine
+brew cask install spotify
+brew cask install evernote
+brew cask install kindle
+brew cask install coconutbattery
