ab411d6d07a21eea9d1b82f14f43aa083d91ddbd
Gabriel Godoy <ggodoyoliveira@gmail.com>
2016-04-15 00:40:33 -0300

    Add new apps to setup machine script

--- setup-machine.sh
+++ setup-machine.sh
@@ -5,26 +5,30 @@
 brew install wget
 brew install node
 brew install imagemagick
+brew install zsh
+brew install zsh-completions
 #
 # Some cask packages that I like.
 #
-brew cask install sublime-text
-brew cask install macvim
-brew cask install iterm2
-brew cask install sketch
-brew cask install dropbox
-brew cask install skitch
+brew cask install atext
+brew cask install alfred
+brew cask install bettertouchtool
+brew cask install caffeine
 brew cask install chitchat
+brew cask install coconutbattery
+brew cask install divvy
+brew cask install dropbox
+brew cask install evernote
 brew cask install firefox
 brew cask install google-chrome
 brew cask install google-drive
-brew cask install livereload
-brew cask install bettertouchtool
-brew cask install atext
-brew cask install alfred
-brew cask install divvy
-brew cask install caffeine
-brew cask install spotify
-brew cask install evernote
+brew cask install iterm2
 brew cask install kindle
-brew cask install coconutbattery
+brew cask install livereload
+brew cask install macvim
+brew cask install sketch
+brew cask install skitch
+brew cask install skype
+brew cask install spotify
+brew cask install sublime-text
+brew cask install vlc
